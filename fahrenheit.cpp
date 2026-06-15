341fe8074e14652bab1b57ffbd554504624fad17
Sameer Khan <sameer295kh@gmail.com>
2026-06-15 19:10:04 +0530

    Add files via upload

---
+++ fahrenheit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int celsius = 0;
+    float fahrenheit = (9 / 5.0 * celsius) + 32;
+    cout << "Fahrenheit value of 0°C  = " << fahrenheit << " F" << endl;
+    return 0;
+}
